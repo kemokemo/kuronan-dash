9cc39fa74c976a95ae6fc5af7bcfd61bdb671cb4
KemoKemo <t2wonderland@gmail.com>
2017-06-13 01:21:46 +0900

    経過時間に従ってうごくアニメーション構造体も追加

--- lib/animation.go
+++ lib/animation.go
@@ -3,13 +3,14 @@ package kuronandash
 import (
 	"fmt"
 	"sync"
+	"time"
 
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
 // StepAnimation is an animation.
-// This animates according to step number information.
+// This animates according to the number of steps.
 type StepAnimation struct {
 	ImagesPaths     []string
 	DurationSteps   int
@@ -17,50 +18,102 @@ type StepAnimation struct {
 	frames          []*ebiten.Image
 	maxFrameNum     int
 	currentFrameNum int
-	totalSteps      int
+	walkedSteps     int
 }
 
-// Init loads asset images and initializes private parameters.
-func (a *StepAnimation) Init() (err error) {
-	a.once.Do(func() {
-		if a.ImagesPaths == nil || len(a.ImagesPaths) == 0 {
-			err = fmt.Errorf("paths is empty, please set valid path info of images")
-			return
-		}
-		a.frames = []*ebiten.Image{}
-		var image *ebiten.Image
-		for _, path := range a.ImagesPaths {
-			image, _, err = ebitenutil.NewImageFromFile(path, ebiten.FilterNearest)
-			if err != nil {
-				return
-			}
-			a.frames = append(a.frames, image)
-		}
-		a.maxFrameNum = len(a.ImagesPaths)
+// Init loads images and initializes private parameters.
+// If you call this function multiple times, it is only the
+// first time to load the images.
+func (s *StepAnimation) Init() (err error) {
+	s.once.Do(func() {
+		s.frames, err = loadImages(s.ImagesPaths)
+		s.maxFrameNum = len(s.ImagesPaths)
 	})
 	if err != nil {
 		return err
 	}
-
-	a.currentFrameNum = 0
-	a.totalSteps = 0
-
+	s.currentFrameNum = 0
+	s.walkedSteps = 0
 	return nil
+}
+
+func loadImages(paths []string) ([]*ebiten.Image, error) {
+	if paths == nil || len(paths) == 0 {
+		err := fmt.Errorf("paths is empty, please set valid path info of images")
+		return nil, err
+	}
+	frames := []*ebiten.Image{}
+	for _, path := range paths {
+		image, _, err := ebitenutil.NewImageFromFile(path, ebiten.FilterNearest)
+		if err != nil {
+			return nil, err
+		}
+		frames = append(frames, image)
+	}
+	return frames, nil
+}
+
+// AddStep adds steps information. If your character moved, please
+// call this function with steps information.
+func (s *StepAnimation) AddStep(steps int) {
+	s.walkedSteps += steps
 }
 
 // GetCurrentFrame returns a current frame image. This function determines
 // the current frame based on the information on how far a character moved.
-// As steps variables, please set the number of steps since calling this
-// function last time. If the sum of steps is grater than the DurationSteps,
-// this function will return the next frame.
-func (a *StepAnimation) GetCurrentFrame(steps int) *ebiten.Image {
-	a.totalSteps += steps
-	if a.totalSteps > a.DurationSteps {
-		a.currentFrameNum++
-		a.totalSteps = 0
+// If the sum of steps is grater than the DurationSteps, this function will
+// return the next frame.
+func (s *StepAnimation) GetCurrentFrame() *ebiten.Image {
+	if s.walkedSteps > s.DurationSteps {
+		s.currentFrameNum++
+		s.walkedSteps = 0
 	}
-	if a.currentFrameNum < 0 || a.maxFrameNum-1 < a.currentFrameNum {
-		a.currentFrameNum = 0
+	if s.currentFrameNum < 0 || s.maxFrameNum-1 < s.currentFrameNum {
+		s.currentFrameNum = 0
 	}
-	return a.frames[a.currentFrameNum]
+	return s.frames[s.currentFrameNum]
+}
+
+// TimeAnimation is an animation.
+// This animates according to elapsed time.
+type TimeAnimation struct {
+	ImagesPaths     []string
+	DurationSecond  float64
+	once            sync.Once
+	frames          []*ebiten.Image
+	maxFrameNum     int
+	currentFrameNum int
+	switchedTime    time.Time
+	elapsed         float64
+}
+
+// Init loads asset images and initializes private parameters.
+func (t *TimeAnimation) Init() (err error) {
+	t.once.Do(func() {
+		t.frames, err = loadImages(t.ImagesPaths)
+		t.maxFrameNum = len(t.ImagesPaths)
+	})
+	if err != nil {
+		return err
+	}
+	t.currentFrameNum = 0
+	t.switchedTime = time.Now()
+	t.elapsed = 0.0
+	return nil
+}
+
+// GetCurrentFrame returns a current frame image. This function determines
+// the current frame according to elapsed time.
+// If the elapsed time is grater than the DurationSecond, this function
+// will return the next frame.
+func (t *TimeAnimation) GetCurrentFrame() *ebiten.Image {
+	t.elapsed = time.Now().Sub(t.switchedTime).Seconds()
+	if t.elapsed >= t.DurationSecond {
+		t.currentFrameNum++
+		t.switchedTime = time.Now()
+	}
+	if t.currentFrameNum < 0 || t.maxFrameNum-1 < t.currentFrameNum {
+		t.currentFrameNum = 0
+	}
+	return t.frames[t.currentFrameNum]
 }
