3c119bc10e164f34c52e87b3e319c61632f55f8b
kemokemo <t2wonderland@gmail.com>
2020-02-17 08:33:02 +0900

    feat: change the font for select scene.

--- assets/messages/description.go
+++ assets/messages/description.go
@@ -2,9 +2,9 @@ package messages
 
 const (
 	// DescKurona is the description for the Kurona
-	DescKurona = "本作の主人公。いつも元気いっぱいな渋垣のアイドル。みんな友達。雷が苦手。"
+	DescKurona = "このゲーム の しゅじんこう。いつもげんきいっぱいな しぶがき の アイドル。みんなともだち。かみなり が にがて。"
 	// DescKoma is the description for the Koma
-	DescKoma = "黒菜の親友。実直な性格で特技は独楽流星落下撃。可愛い。水が苦手。"
+	DescKoma = "くろな の しんゆう。じっちょく な せいかく で とくぎ は こまりゅうせいらっかげき。かわいい。みず が にがて。"
 	// DescShishimaru is the description for the Shishimaru
-	DescShishimaru = "優しく穏やかな性格だが、いざという時には凛々しい一面も。独楽の弟。料理が上手。"
+	DescShishimaru = "やさしく おだやかな せいかくだが、いざというときには りりしい いちめん も。こま の おとうと。りょうり が じょうず。"
 )
