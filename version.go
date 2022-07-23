1564ddaa349a900c42693e83c452a2504de77847
kemokemo <t2wonderland@gmail.com>
2022-07-23 19:22:53 +0900

    chore: v0.5.0

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package main
 
 // Version is the semantic version of this game.
-var Version = "v0.0.4"
+var Version = "v0.0.5"
 
 // Revision is the revision built with this game.
 var Revision = ""
