5376507ba1c0ef836e00c6628eda679ebe454770
kemokemo <t2wonderland@gmail.com>
2023-01-20 22:56:46 +0900

    chore: add cspell hack

--- verify.sh
+++ verify.sh
@@ -1,25 +1,31 @@
 #!/bin/sh
 
+cspell lint '**/*.go' -c ./.cspell/cspell.cjs --gitignore --show-suggestions --no-progress
+if [ $? -ne 0 ]; then
+    echo "## failed to run 'cspell'. ##"
+    exit $?
+fi
+
 go vet -v ./...
 if [ $? -ne 0 ]; then
-    echo "failed to run 'go vet'."
+    echo "## failed to run 'go vet'. ##"
     exit $?
 fi
 
 golangci-lint run
 if [ $? -ne 0 ]; then
-    echo "failed to run 'golangci-lint'."
+    echo "## failed to run 'golangci-lint'. ##"
     exit $?
 fi
 
 go test -v ./...
 if [ $? -ne 0 ]; then
-    echo "failed to run 'go test'."
+    echo "## failed to run 'go test'. ##"
     exit $?
 fi
 
 go build
 if [ $? -ne 0 ]; then
-    echo "failed to run 'go build'."
+    echo "## failed to run 'go build'. ##"
     exit $?
 fi
